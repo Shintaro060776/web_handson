c32ed40a46770ab5c1ca622775df73806a4c97bd
shin20090317 <shintaro060776@gmail,com>
2023-07-31 17:22:51 +0900

    first commit

---
+++ Javascript/基本編/まずは始めてみよう/script.js
@@ -0,0 +1,5 @@
+// document.getElementById("date-container").innerHTML = new Date().toLocaleString();
+
+setInterval(functions() {
+    document.getElementById("d2").innerHTML = new Date().toLocaleString();
+}, 1000);
