0349b6d681f082313d5fc95f4d5f92106a3aacbd
Shintaro060776 <shintaro060776@gmail.com>
2023-08-10 13:47:32 +0900

    Delete Bash directory

--- Bash/question2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p  "Please enter any value here" value
-
-a=$value
-
-echo "$a"
--- Bash/question8.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-count=5
-
-until [ $count -lt 1 ]; do
-        echo $count
-        count=$((count - 1))
-done
